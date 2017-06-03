317ca3b88d0a96fb30cefb91875b01c5575ecfa7
azizsabuj <azizsabuj@gmail.com>
2017-06-03 17:43:13 +0600

    3rd commit

---
+++ URI - 1005 - Average 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double A,B,MEDIA;
+    float C=3.5,D=7.5;
+    scanf("%lf%lf", &A, &B);
+    MEDIA = ((A*C)+(B*D))/(C+D);
+    printf("MEDIA = %.5lf\n", MEDIA);
+    return 0;
+}
